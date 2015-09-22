13891a8acbf4573709f87cc86f95bb0eddc96b79
Andrew Zaplitnyak <zaplitnyak@gmail.com>
2015-09-22 23:48:45 +0300

    norm view, first funcs done

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,9 @@
 ;(function($) {
 $(function() {
+    $("p.splt-heading").click(function() {
+        window.location = "/menu.html"
+    });
+    
     $("#repeat-button").click(function() {
         $("iframe#playBack").prop('src', $("iframe#playBack").prop('src'));
     });
@@ -9,20 +13,23 @@ $(function() {
         // get values from FORM
         var message = $("textarea#message").val();
         if (!message) {
-            $()
-            
+            $(".help-block").text($("textarea#message").data("validationRequiredMessage"));
+            $(".help-block").show();
+
             return false;
         }
-        var secretUrl = "https://translate.google.com.ua/translate_tts?ie=UTF-8&tl=ru&total=1&idx=0&textlen=30&tk=878821&client=t&prev=input&q=";
+        var secretUrl = "https://translate.google.com.ua/translate_tts?ie=UTF-8&tl=ru&client=t&q=";
         var $playBackFrame = $("iframe#playBack");
         var generatedUrl = secretUrl + encodeURIComponent(message);
         $playBackFrame.prop('src', generatedUrl);
         success(generatedUrl);
+        $("#download-button").prop("href", generatedUrl);
+        $("#download-button").prop("download", message.substring(0, 10) + ".mp3");
     });
 
     function success(generatedUrl) {
             // Success message
-            $('#success').html("<div class='alert alert-success'>");
+            $('#success').html("<div class='alert alert-success no_margin'>");
             $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                 .append("</button>");
             $('#success > .alert-success')
@@ -37,7 +44,7 @@ $(function() {
 /*When clicking on Full hide fail/success boxes */
 $('#message').focus(function() {
     $('#success').html('');
-
+    $(".help-block").hide();
     //clear all fields
     $('#spellItForm').trigger("reset");
 });
