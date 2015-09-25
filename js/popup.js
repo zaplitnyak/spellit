bbc8aebb0428d42d276e0162dd060a40c03a354c
Andrew Zaplitnyak <zaplitnyak@gmail.com>
2015-09-25 23:04:45 +0300

    iframe to audio window size

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,25 @@
 ;(function($) {
-    $(function() {
-        var $messageTextArea = $('#message'),
-            $spltForm = $("#spellItForm"),
-            $playBackFrame = $("iframe#playBack"),
-            text = getUrlParameterByName("text");
+    var $messageTextArea = $('#message'),
+        $spltForm = $("#spellItForm"),
+        $playBackAudio = $("audio#playBack"),
+        $startButton = $("#start-button"),
+        $stopButton = $("#stop-button"),
+        text = getUrlParameterByName("text");
 
-        $playBackFrame.prop("src", "");
+    $(function() {
+        $playBackAudio.prop("src", "");
+
+        $playBackAudio.on("ended", function () {
+            stopPlayback();
+        });
+
+        $stopButton.click(function () {
+            stopPlayback();
+        });
 
         $messageTextArea.focus(function() {
             $('#success').hide();
             $(".help-block").hide();
-            $('#spellItForm').trigger("reset");
         });
 
         $("p.splt-heading").click(function() {
@@ -18,29 +27,31 @@
         });
 
         $("#repeat-button").click(function() {
-            $("iframe#playBack").prop('src', $("iframe#playBack").prop('src'));
+            startPlayback();
         });
 
         $spltForm.submit(function(event) {
             event.preventDefault();
 
-            var secretUrl = "https://translate.google.com.ua/translate_tts?ie=UTF-8&tl=ru&client=t&q=",
+            var secretUrl = "https://translate.google.com.ua/translate_tts?ie=UTF-8&tl=ru&total=1&idx=0&client=t",
                 message = $("textarea#message").val();
             if (!message) {
+                //TODO:Implement jQuery Templates
                 $(".help-block").text($("textarea#message").data("validationRequiredMessage"));
                 $(".help-block").show();
 
                 return false;
             }
 
-            var generatedUrl = secretUrl + encodeURIComponent(message);
-            $playBackFrame.prop('src', generatedUrl);
+            var generatedUrl = secretUrl + "&textlen=" + message.length + "&q=" + encodeURIComponent(message);
+            $playBackAudio.prop('src', generatedUrl);
+            startPlayback();
             var resultMessage = "<strong>Бесплатная ссылка для прослушивания: </strong><br/>" +
                     "<a target=\"_blank\" href=\"" + generatedUrl + "\">" + generatedUrl + "</a>";
             successNotification(resultMessage);
             $("#download-button").prop("href", generatedUrl);
             $("#download-button").prop("download", message.substring(0, 10) + ".mp3");
-            saveRecentVoices(message);
+            saveToRecentVoices(message);
         });
 
         if (text) {
@@ -51,6 +62,7 @@
         }
     });
 
+    //TODO:Implement jQuery Templates
     function successNotification(message) {
         $('#success').html("<div class='alert alert-success splt-success'>");
         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -62,7 +74,7 @@
         $('#success').show();
     }
 
-    function saveRecentVoices(message) {
+    function saveToRecentVoices(message) {
         chrome.storage.sync.get("recentVoices", function (data) {
             var recentVoices = (Object.keys(data).length) ? data.recentVoices : [];
 
@@ -81,6 +93,19 @@
             results = regex.exec(window.location.search);
 
         return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+
+    function startPlayback() {
+        $playBackAudio.trigger("play");
+        $startButton.hide();
+        $stopButton.show();
+    }
+
+    function stopPlayback() {
+        $playBackAudio.trigger("pause");
+        $playBackAudio.prop("currentTime", 0);
+        $stopButton.hide();
+        $startButton.show();
     }
 
 })(window.jQuery);
